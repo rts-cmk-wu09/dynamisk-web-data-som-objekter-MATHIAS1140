1d55a17929999707efcbb624bc769de080f095b2
MATHIAS1140 <U2420682@RTS-365.DK>
2023-02-07 12:15:43 +0100

    add - Dynamisk web - data som objekter

---
+++ script.js
@@ -0,0 +1,17 @@
+let person = {
+    firstname: "ole",
+    alder: "31",
+    skole: "roskilde tek",
+    år: "2022"
+}
+
+console.log(person.firstname)
+console.log(person.alder)
+console.log(person.skole)
+console.log(person.år)
+
+console.log(person["firstname"])
+console.log(person["alder"])
+console.log(person["skole"])
+console.log(person["år"])
+
