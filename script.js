c230cf55cf11ca40c2f7f3ee1a8e1ed7d2e31440
MATHIAS1140 <U2420682@RTS-365.DK>
2023-02-07 13:33:38 +0100

    add objekt opgave 2

--- script.js
+++ script.js
@@ -15,3 +15,36 @@ console.log(person["alder"])
 console.log(person["skole"])
 console.log(person["år"])
 
+
+
+let nyPerson = {
+    firstname: "ole",
+    alder: "31",
+    skole: "roskilde tek",
+    år: "2022",
+    gift: false,
+    hobbyer: [{
+        Sport: "fodbold", skema: {
+            onsdag: "kl: 18:00",
+            fredag: "kl: 18:00",
+            søndag: "kl: 12:30"
+        }
+    },
+    {
+        sport: "Tennis", skema: {
+            tirsdag: "kl: 18:00",
+            fredag: "kl: 14:00",
+            lørdag: "kl: 12:30"
+        }
+    },
+    {
+        sport: "Håndbold", skema: {
+            mandag: "kl: 18:00",
+            tirsdag: "kl: 12:00"
+        }
+    }]
+}
+
+console.log(nyPerson)
+
+nyPerson.hobbyer.forEach()
