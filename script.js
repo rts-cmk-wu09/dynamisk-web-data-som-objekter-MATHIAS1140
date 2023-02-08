be79ef8450f4cb3e263c5fb9d3960fa2616b52f7
MATHIAS1140 <U2420682@RTS-365.DK>
2023-02-08 08:57:36 +0100

    add opgave 3 færdig

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//opgave 1
 let person = {
     firstname: "ole",
     alder: "31",
@@ -14,37 +15,49 @@ console.log(person["firstname"])
 console.log(person["alder"])
 console.log(person["skole"])
 console.log(person["år"])
+//opgave 1 slut
 
 
 
+//opgave 2
 let nyPerson = {
     firstname: "ole",
     alder: "31",
     skole: "roskilde tek",
     år: "2022",
     gift: false,
-    hobbyer: [{
-        Sport: "fodbold", skema: {
-            onsdag: "kl: 18:00",
-            fredag: "kl: 18:00",
-            søndag: "kl: 12:30"
-        }
-    },
-    {
-        sport: "Tennis", skema: {
-            tirsdag: "kl: 18:00",
-            fredag: "kl: 14:00",
-            lørdag: "kl: 12:30"
-        }
-    },
-    {
-        sport: "Håndbold", skema: {
-            mandag: "kl: 18:00",
-            tirsdag: "kl: 12:00"
-        }
-    }]
+    hobbyer:
+        [{
+            sport: "fodbold", skema: {
+                onsdag: "kl: 18:00",
+                fredag: "kl: 18:00",
+                søndag: "kl: 12:30"
+            }
+        },
+        {
+            sport: "Tennis", skema: {
+                tirsdag: "kl: 18:00",
+                fredag: "kl: 14:00",
+                lørdag: "kl: 12:30"
+            }
+        },
+        {
+            sport: "Håndbold", skema: {
+                mandag: "kl: 18:00",
+                tirsdag: "kl: 12:00"
+            }
+        }]
 }
 
 console.log(nyPerson)
 
-nyPerson.hobbyer.forEach()
+// => er en nemmere måde at skrive ordet -function- på - en forkortelse af syntaksen.
+nyPerson.hobbyer.forEach(hobby => {
+    console.log(hobby)
+
+})
+
+console.log(nyPerson.hobbyer[1].skema.lørdag)
+
+
+//opgave 2 slut
